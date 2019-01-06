5b9ad5195c616e2d814c207630da97233c8d4de9
Peter Mitrano <mitranopeter@gmail.com>
2019-01-06 17:06:52 -0500

    78

--- 78.cpp
+++ 78.cpp
@@ -1,44 +1,53 @@
 #include <iostream>
+#include <map>
+
+// to be honest, this is cheating because I know we don't need k > 500
+std::array<unsigned long, 500> pentagonal;
 
 unsigned long partition(unsigned long n) {
-    if (n == 0) {
-        return 1;
-    } else if (n == 1) {
-        return 1;
-    } else if (n == 2) {
-        return 2;
+    static std::map<unsigned long, unsigned long> memoization = {{0, 1},
+                                                                 {1, 1}};
+    auto const it = memoization.find(n);
+    if (it != memoization.cend()) {
+        return it->second;
     }
-    auto sum{0ul};
-    auto pentagonal = [](int k) {
-        return static_cast<unsigned long>(k * (3 * k - 1) / 2);
-    };
-    auto k{1};
-    while (true) {
-        auto const sign = k % 2 == 0 ? -1 : 1;
-        auto const pent = pentagonal(k);
+
+    auto partitions{0l};
+    for (auto k{1};; ++k) {
+        auto const sign = ((k + 1) / 2) % 2 == 0 ? -1 : 1;
+        auto const pent = pentagonal[k];
         if (n < pent) {
             break;
         }
-        sum += sign * partition(n - pent);
+        partitions += sign * partition(n - pent);
+    }
+    partitions = partitions % 1'000'000;
+    if (partitions < 0) {
+        partitions += 1'000'000;
+    }
+    memoization.emplace(n, partitions);
+    return static_cast<unsigned long>(partitions);
+}
+
+unsigned long problem() {
+    // pre-compute pentagonal numbers
+    auto k{1};
+    for (auto j{1ul}; j < pentagonal.size(); ++j) {
+        pentagonal[j] = static_cast<unsigned long>(k * (3 * k - 1) / 2);
         if (k > 0) {
             k = -k;
         } else {
             k = -k + 1;
         }
     }
-    return sum;
-}
 
-unsigned long problem() {
     auto n{0ul};
     while (true) {
         auto const p = partition(n);
-        std::cout << n << " " << p << '\n';
-        ++n;
-        if (p > 1'000'000ul) {
-            break;
+        if (p == 0) {
+            return n;
         }
+        ++n;
     }
 
-    return 0ul;
 }
